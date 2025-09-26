faa95d61da1183b721d771cb9659009f8b6fec24
Ahmed <ahmedmedhat749d1@gmail.com>
2025-09-26 20:24:50 +0300

    update

---
+++ build/arabian_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/E_Disk/arabian_ros/ws/intern_ws/src/arabian_robot
+# Build directory: /home/ahmed/E_Disk/arabian_ros/ws/intern_ws/build/arabian_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
